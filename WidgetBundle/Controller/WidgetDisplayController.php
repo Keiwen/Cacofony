da3351a81854f0231293f801234b2b605b75ce12
Keiwen <romainwald@hotmail.com>
2016-11-29 18:39:42 +0100

    added get parameter for widget display

--- WidgetBundle/Controller/WidgetDisplayController.php
+++ WidgetBundle/Controller/WidgetDisplayController.php
@@ -6,6 +6,7 @@ namespace Keiwen\Cacofony\WidgetBundle\Controller;
 use Keiwen\Cacofony\Controller\AppController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
 
 class WidgetDisplayController extends AppController
 {
@@ -14,9 +15,10 @@ class WidgetDisplayController extends AppController
      * @Route("/{widget}", name="displayWidget")
      * @Template("KeiwenCacofonyWidgetBundle:WidgetDisplay:displayWidget.html.twig")
      */
-    public function displayWidgetAction($widget)
+    public function displayWidgetAction(Request $request, $widget)
     {
         $this->addTemplateParam('widgetName', $widget);
+        $this->addTemplateParam('widgetParam', $request->query->all());
         return $this->renderTemplate();
     }
 
