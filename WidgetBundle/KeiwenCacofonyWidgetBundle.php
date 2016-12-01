81f2900b8c2741c6617342e6d02cdb5c23b17c6e
Keiwen <romainwald@hotmail.com>
2016-12-01 14:12:25 +0100

    switch to multiple widget controller

--- WidgetBundle/KeiwenCacofonyWidgetBundle.php
+++ WidgetBundle/KeiwenCacofonyWidgetBundle.php
@@ -2,6 +2,7 @@
 
 namespace Keiwen\Cacofony\WidgetBundle;
 
+use Keiwen\Cacofony\WidgetBundle\Controller\WidgetController;
 use Keiwen\Cacofony\WidgetBundle\DependencyInjection\Compiler\WidgetCompiler;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
@@ -9,7 +10,8 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 class KeiwenCacofonyWidgetBundle extends Bundle
 {
 
-    const CONTROLLER_CLASSNAME = 'Controller\\WidgetController';
+    const CONTROLLER_PATH = 'Controller';
+    const CONTROLLER_DEFAULT_CLASSNAME = 'WidgetController';
     const TWIGEXT_CLASSNAME = 'Twig\\TwigWidget';
 
     public function build(ContainerBuilder $container)
@@ -17,20 +19,48 @@ class KeiwenCacofonyWidgetBundle extends Bundle
         parent::build($container);
 
         $container->addCompilerPass(
-            new WidgetCompiler(static::getControllerClassName(), static::getTwigExtensionClassName(), static::getBundleName())
+            new WidgetCompiler(static::getTwigExtensionClassName(), static::class)
         );
     }
 
 
     /**
+     * @param string $name widget controller name
      * @return string
      */
-    protected static function getControllerClassName()
+    public static function getControllerClassName(string $name = '')
     {
-        $static = static::getBundleNamespace() . '\\' . static::CONTROLLER_CLASSNAME;
-        if(class_exists($static)) return $static;
-        return static::getDefaultBundleNamespace() . '\\' . self::CONTROLLER_CLASSNAME;
+        if(empty($name)) $name = static::CONTROLLER_DEFAULT_CLASSNAME;
+        //be sure to end controller name by Controller
+        if(!preg_match('#(.*)Controller$#', $name)) $name .= 'Controller';
+
+        //try static (child bundle)
+        $static = static::getBundleNamespace();
+        if(!empty(static::CONTROLLER_PATH)) $static .= '\\' . static::CONTROLLER_PATH;
+        $static .= '\\' . ucfirst($name);
+        if(class_exists($static)) {
+            //check if subclass
+            if(is_subclass_of($static, WidgetController::class)) {
+                return $static;
+            }
+            throw new \RuntimeException(sprintf('Controller "%s" used as widget controller must extends "%s"', $static, WidgetController::class));
+        }
+
+        //try self (parent bundle)
+        $self = self::getDefaultBundleNamespace();
+        if(!empty(self::CONTROLLER_PATH)) $self .= '\\' . self::CONTROLLER_PATH;
+        $self .= '\\' . ucfirst($name);
+        if(class_exists($self)) {
+            //check if subclass
+            if(is_subclass_of($self, WidgetController::class)) {
+                return $self;
+            }
+            throw new \RuntimeException(sprintf('Controller "%s" used as widget controller must extends "%s"', $self, WidgetController::class));
+        }
+
+        throw new \RuntimeException(sprintf('Controller "%s" not found (looked for [%s] and [%s])', $name, $static, $self));
     }
+
 
     /**
      * @return string
