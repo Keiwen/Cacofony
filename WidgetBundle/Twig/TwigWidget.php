79b4e47e512f180aa2471ad6aa7a724f2d02680e
Keiwen <romainwald@hotmail.com>
2016-12-01 14:11:14 +0100

    switch to multiple widget controller

--- WidgetBundle/Twig/TwigWidget.php
+++ WidgetBundle/Twig/TwigWidget.php
@@ -7,6 +7,7 @@ use Keiwen\Cacofony\Reader\TemplateAnnotationReader;
 use Keiwen\Cacofony\WidgetBundle\Controller\WidgetController;
 use Keiwen\Cacofony\WidgetBundle\Exception\UnhandledWidgetRendererException;
 use Keiwen\Cacofony\WidgetBundle\Exception\WidgetNotFoundException;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Response;
 
 class TwigWidget extends \Twig_Extension
@@ -19,16 +20,16 @@ class TwigWidget extends \Twig_Extension
 
     /** @var WidgetController */
     protected $controller;
-    /** @var TemplateAnnotationReader */
-    protected $annotationReader;
-    protected $bundleName;
+    /** @var ContainerInterface */
+    protected $container;
+    protected $bundleClassName;
 
-    public function __construct(WidgetController $controller, string $bundleName)
+    protected static $controllerInstances = array();
+
+    public function __construct(ContainerInterface $container, string $bundleClassName)
     {
-        $this->controller = $controller;
-        $this->annotationReader = new TemplateAnnotationReader($controller, $bundleName);
-        $this->annotationReader->setMethodSuffix('Widget');
-        $this->bundleName = $bundleName;
+        $this->container = $container;
+        $this->bundleClassName = $bundleClassName;
     }
 
     /**
@@ -70,16 +71,17 @@ class TwigWidget extends \Twig_Extension
      */
     public function callWidget(string $widget, array $parameters = array())
     {
-        $method = $widget . 'Widget';
+        list($controllerName, $widgetName) = $this->readControllerWidgetName($widget);
+        $this->controller = $this->loadController($controllerName);
+        $method = $widgetName . 'Widget';
         if(!method_exists($this->controller, $method)) {
-            $method = $widget;
+            $method = $widgetName;
             if(!method_exists($this->controller, $method)) {
                 throw new WidgetNotFoundException(
-                    sprintf('No method for rendering widget "%s" in controller %s', $widget, get_class($this->controller))
+                    sprintf('No method for rendering widget "%s" in controller %s', $widgetName, get_class($this->controller))
                 );
             }
         }
-        $this->controller->setAutodumpParamWidgetSuffix($widget);
 
         $this->controller->setWidgetParameters($parameters);
         $widgetReturn = $this->controller->$method();
@@ -103,7 +105,7 @@ class TwigWidget extends \Twig_Extension
                                     string $loaderVersion = '',
                                     string $loadErrorVersion = '')
     {
-        $this->controller->setAutodumpParamWidgetSuffix('asyncLoader');
+        $this->controller = $this->loadController();
         $widgetReturn = $this->controller->asyncLoaderWidget($url, $parameters, $method, $loaderVersion, $loadErrorVersion);
         return $this->renderWidget($widgetReturn, 'asyncLoaderWidget');
     }
@@ -122,7 +124,8 @@ class TwigWidget extends \Twig_Extension
                                          string $loaderVersion = '',
                                          string $loadErrorVersion = '')
     {
-        $url = $this->controller->generateWidgetRouteUrl($routeName, $parameters);
+        $controller = $this->loadController();
+        $url = $controller->generateWidgetRouteUrl($routeName, $parameters);
         return $this->callAsyncWidget($url, $parameters, $method, $loaderVersion, $loadErrorVersion);
     }
 
@@ -139,7 +142,11 @@ class TwigWidget extends \Twig_Extension
         switch(true) {
             case is_array($widgetReturn):
                 //widget send back template parameters only, should have template annotation
-                $template = $this->annotationReader->getTemplateFromAnnotation($methodCalled);
+                $bundleName = explode('\\', $this->bundleClassName);
+                $bundleName = array_pop($bundleName);
+                $annotationReader = new TemplateAnnotationReader($this->controller, $bundleName);
+                $annotationReader->setMethodSuffix('Widget');
+                $template = $annotationReader->getTemplateFromAnnotation($methodCalled);
                 return $this->controller->renderWidgetContent($template, $widgetReturn);
             case is_string($widgetReturn);
                 //consider html content directly provided
@@ -161,5 +168,33 @@ class TwigWidget extends \Twig_Extension
         throw new UnhandledWidgetRendererException(sprintf($error, $methodCalled, get_class($this->controller)));
     }
 
+
+    /**
+     * @param string $name
+     * @return WidgetController
+     */
+    protected function loadController(string $name = '')
+    {
+        $controller = ($this->bundleClassName)::getControllerClassName($name);
+        if(empty(static::$controllerInstances[$name])) {
+            static::$controllerInstances[$name] = new $controller($this->container);
+        }
+        return static::$controllerInstances[$name];
+    }
+
+
+    /**
+     * @param string $name
+     * @return array
+     */
+    protected function readControllerWidgetName(string $name)
+    {
+        $matches = array();
+        $pattern = '#(.*):(.*)#';
+        preg_match($pattern, $name, $matches);
+        $controllerName = empty($matches[1]) ? '' : $matches[1];
+        $widgetName = empty($matches[2]) ? $name : $matches[2];
+        return array($controllerName, $widgetName);
+    }
 
 }
