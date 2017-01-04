a2e7f8c8bbffb83d911764ea1392aa51631de7e4
Keiwen <romainwald@hotmail.com>
2017-01-04 09:59:52 +0100

    adding role prefixes conf

--- DependencyInjection/KeiwenCacofonyExtension.php
+++ DependencyInjection/KeiwenCacofonyExtension.php
@@ -13,7 +13,9 @@ class KeiwenCacofonyExtension extends ConfigurableExtension
 
     const API_PARAMETERS_CONF = 'keiwen_cacofony.api_parameters';
     const CONTROLLER_CONF = 'keiwen_cacofony.controller';
-
+    const PARAM_FETCHER_LISTENER_PRIORITY_CONF = 'keiwen_cacofony.param_fetcher_listener.priority';
+    const PARAM_FETCHER_CONTROLLER_PARAM_CONF = 'keiwen_cacofony.param_fetcher.controller_parameter';
+    const ROLE_PREFIXES_CONF = 'keiwen_cacofony.role_prefixes';
 
     public function loadInternal(array $mergedConfig, ContainerBuilder $container)
     {
@@ -23,13 +25,12 @@ class KeiwenCacofonyExtension extends ConfigurableExtension
             new FileLocator(__DIR__.'/../Resources/config')
         );
 
-        $apiParameters = $mergedConfig['api_parameters'];
-        $container->setParameter(self::API_PARAMETERS_CONF, $apiParameters);
-        $controllerParameters = $mergedConfig['controller'];
-        $container->setParameter(self::CONTROLLER_CONF, $controllerParameters);
-
-        $container->setParameter('keiwen_cacofony.param_fetcher_listener.priority', $mergedConfig['param_fetcher']['listener_priority']);
-        $container->setParameter('keiwen_cacofony.param_fetcher.controller_parameter', $mergedConfig['param_fetcher']['controller_parameter']);
+        $container->setParameter(self::API_PARAMETERS_CONF, $mergedConfig['api_parameters']);
+        $container->setParameter(self::CONTROLLER_CONF, $mergedConfig['controller']);
+        if(!isset($mergedConfig['rolechecker']['role_prefixes'])) $mergedConfig['rolechecker']['role_prefixes'] = array();
+        $container->setParameter(self::ROLE_PREFIXES_CONF, $mergedConfig['rolechecker']['role_prefixes']);
+        $container->setParameter(self::PARAM_FETCHER_LISTENER_PRIORITY_CONF, $mergedConfig['param_fetcher']['listener_priority']);
+        $container->setParameter(self::PARAM_FETCHER_CONTROLLER_PARAM_CONF, $mergedConfig['param_fetcher']['controller_parameter']);
 
         $loader->load('services_entityRegistry.yml');
         $loader->load('services_paramFetcher.yml');
