7dee751b511fed45921c293967071e27b28c0c02
Keiwen <romainwald@hotmail.com>
2017-02-13 11:58:28 +0100

    create 423 and 500 http exceptions

---
+++ HttpException/ServerErrorHttpException.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace Keiwen\Cacofony\HttpException;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+/**
+ * ServerErrorHttpException.
+ */
+class ServerErrorHttpException extends HttpException
+{
+    /**
+     * Constructor.
+     *
+     * @param string     $message  The internal exception message
+     * @param \Exception $previous The previous exception
+     * @param int        $code     The internal exception code
+     */
+    public function __construct($message = null, \Exception $previous = null, $code = 0)
+    {
+        parent::__construct(Response::HTTP_INTERNAL_SERVER_ERROR, $message, $previous, array(), $code);
+    }
+}
