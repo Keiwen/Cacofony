75135d57ce6fdc6efa2c612fe01e2c8a0433c927
Keiwen <romainwald@hotmail.com>
2017-04-20 11:38:14 +0200

    add login time out http exception (440, not official)

---
+++ Exception/LoginTimeoutHttpException.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Keiwen\Cacofony\Exception;
+
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+class LoginTimeoutHttpException extends HttpException
+{
+
+    const HTTP_CODE = 440;
+
+    /**
+     * Constructor.
+     *
+     * @param string     $message  The internal exception message
+     * @param \Exception $previous The previous exception
+     * @param int        $code     The internal exception code
+     */
+    public function __construct($message = null, \Exception $previous = null, $code = 0)
+    {
+        parent::__construct(static::HTTP_CODE, $message, $previous, array(), $code);
+    }
+
+}
