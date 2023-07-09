51fe671caf8c53078dede9d4271142dc2b4df2e3
Keiwen <keiwen.mk@gmail.com>
2023-07-09 13:43:03 +0200

    add twig Array filters

---
+++ Twig/TwigArray.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Keiwen\Cacofony\Twig;
+
+use Doctrine\Common\Collections\Collection;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class TwigArray extends AbstractExtension
+{
+
+    /**
+     * @return array
+     */
+    public function getFilters()
+    {
+        return array(
+            new TwigFilter('shuffle', array($this, 'shuffleFilter')),
+        );
+    }
+
+
+    /**
+     * Shuffle input array
+     * @param array|Collection  $array
+     * @return array
+     */
+    public function shuffleFilter(array $array)
+    {
+        if ($array instanceof Collection) {
+            $shuffledArray = $array->toArray();
+        } else {
+            $shuffledArray = $array;
+        }
+        shuffle($shuffledArray);
+        return $shuffledArray;
+    }
+
+
+}
